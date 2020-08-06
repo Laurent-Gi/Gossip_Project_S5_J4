773e2933ed65a225c5c84192a3ced2caffe4b438
Laurent Saives <etrenjeu@gmail.com>
2020-08-07 01:53:30 +0200

    Ajout du seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,38 +16,44 @@ Comment.destroy_all
 
 # Création de 10 utilisateurs :
 # -----------------------------
-10.times do
+puts "Création des Cities :"
+14.times do
   City.create!(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
 end
 
-10.times do
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, email: Faker::Internet.email, age: rand(15..80), city_id: City.all.sample.id)
+puts "Création des Users :"
+15.times do
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, email: Faker::Internet.email, age: rand(15..80), city_id: City.all.sample.id, password: "12345678")
 end
 
-
-20.times do |index| #cree 20 gossip en reference avec user
+puts "Création des Gossips :"
+25.times do |index| #cree 20 gossip en reference avec user
   Gossip.create!(title: "Gossip#{index+1}",content: Faker::ChuckNorris.fact, user: User.all.sample)
 end
 
-10.times do     #cree 10 tag
-  Tag.create(title: "##{Faker::Games::Pokemon.name}")
+puts "Création des Tags :"
+12.times do     #cree 10 tag
+  Tag.create!(title: "##{Faker::Games::Pokemon.name}")
 end
 
-
-10.times do
+puts "Création des PrivatesMessages :"
+12.times do
   PrivateMessage.create!(content: Faker::Lorem.sentence, sender_id: User.all.sample.id, recipient_id: User.all.sample.id)
 end
 
+puts "Création des TagGossips :"
 20.times do
   TagGossip.create!(tag_id: Tag.all.sample.id, gossip_id: Gossip.all.sample.id)
 end
 
+puts "Création supplémentaires pour des tests :"
 # Créations de quelques commentaires de Gossips
 Comment.create!(content: "bravo pour ton gossip !", user: User.first, gossip: Gossip.find_by(user: User.first))
 Comment.create!(content: "c'est une belle journée", user: User.last, gossip: Gossip.find_by(user: User.last))
 
+City.create!(name: "Rennes", zip_code: "35000")
 City.create!(name: "Paris", zip_code: "75014")
 
-User.create!(first_name: "anonymous", last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, email: Faker::Internet.email, age: rand(30..60), city: City.last)
+User.create!(first_name: "anonymous", last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, email: Faker::Internet.email, age: rand(30..60), city: City.last, password: "inconnu")
 Gossip.create!(title: "Pour une fois",content: Faker::ChuckNorris.fact, user: User.last)
 Comment.create!(content: "Et avec du soleil", user: User.last, gossip: Gossip.find_by(user: User.last))
